612e7a3d97cb06b8c70241922b269d7f619ac1a3
蒋军晖 <mobeigege@icloud.com>
2022-03-23 23:34:38 +0800

    初始化构建包,创建基础包,dev、prod包

---
+++ builder-webpack/lib/webpack.base.js
@@ -0,0 +1,135 @@
+const glob = require('glob')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin')
+const path = require('path')
+/* 捕获构建错误 */
+class BuildCompiler {
+	apply(compiler) {
+		compiler.hooks.compilation.tap('done', stats => {
+			console.log(stats)
+		})
+	}
+}
+/* 动态获取入口和模版 */
+const setMPA = (name = 'index') => {
+	const entry = {}
+	const htmlWebpackPlugins = []
+	const entryFiles = glob.sync(path.join(__dirname, `../src/*/${name}.js`))
+	Object.keys(entryFiles).map(index => {
+		const entryFile = entryFiles[index]
+		const regexp = new RegExp(`src/(.*)/${name}.js`)
+		const match = entryFile.match(regexp)
+		const pageName = match && match[1]
+		if (pageName) {
+			entry[pageName] = entryFile
+			htmlWebpackPlugins.push(
+				new HtmlWebpackPlugin({
+					template: path.join(
+						__dirname,
+						`../src/${pageName}/index.html`
+					),
+					filename: `${pageName}.html`,
+					chunks: [pageName],
+					inject: true,
+					minify: {
+						html5: true,
+						collapseWhitespace: true,
+						preserveLineBreaks: false,
+						minifyCSS: true,
+						minifyJS: true,
+						removeComments: false
+					}
+				})
+			)
+		}
+	})
+	return {
+		entry,
+		htmlWebpackPlugins
+	}
+}
+const { entry, htmlWebpackPlugins } = setMPA
+module.exports = {
+	module: {
+		entry,
+		rules: [
+			{
+				test: /.js$/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.less$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'px2rem-loader',
+						options: {
+							remUnit: 75, // 750设计稿等分的基数
+							remPrecision: 8 // 保留8位小数
+						}
+					},
+					'less-loader', // less 解析
+					{
+						loader: 'postcss-loader', // 补全css前缀
+						options: {
+							postcssOptions: {
+								plugins: ['postcss-preset-env']
+							}
+						}
+					}
+				]
+			},
+			/* {
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							// 压缩法
+							limit: 10240
+						}
+					}
+				]
+			}, */
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader'
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: ['file-loader']
+			}
+		]
+	},
+	plugins: [
+		/* css 文件合并  */
+		new MiniCssExtractPlugin({
+			filename: '[name].[contenthash:8].css'
+		}),
+		new CleanWebpackPlugin(),
+		new FriendlyErrorsWebpackPlugin(),
+		function () {
+			this.hooks.done.tap('done', stats => {
+				if (
+					stats.compilation.errors &&
+					stats.compilation.errors.length
+				) {
+					console.log('build error')
+					process.exit(1)
+				}
+			})
+		}
+	].concat(htmlWebpackPlugins),
+	stats: 'errors-only'
+}
---
+++ builder-webpack/lib/webpack.dev.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
+
+const devConfig = {
+	mode: 'development',
+	plugins: [new webpack.HotModuleReplacementPlugin()],
+	devServer: {
+		// 服务器访问的基本目录
+		static: './dist',
+		compress: true,
+		// 端口
+		port: 8080,
+		// 自动打开页面
+		open: true
+	},
+	devtool: 'cheap-module-source-map'
+}
+
+module.exports = merge(baseConfig, devConfig)
